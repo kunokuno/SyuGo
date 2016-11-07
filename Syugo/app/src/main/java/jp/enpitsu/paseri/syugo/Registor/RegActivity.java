797a6b6a91ad5d2b74873f22c44f298b9a36d22d
Kaori <m1671105@ems.u-toyama.ac.jp>
2016-11-07 14:36:20 +0900

    obtain mac_address and change the SNS share message

--- Syugo/app/src/main/java/jp/enpitsu/paseri/syugo/Registor/RegActivity.java
+++ Syugo/app/src/main/java/jp/enpitsu/paseri/syugo/Registor/RegActivity.java
@@ -1,7 +1,10 @@
 package jp.enpitsu.paseri.syugo.Registor;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,22 +20,26 @@ import jp.enpitsu.paseri.syugo.R;
  */
 public class RegActivity extends Activity {
 
-    Button btn_issue;
-    Button btn_share;
-    Button btn_findmode;
-    EditText id_box;
-    TextView text_idshow;
+    private Button btn_issue;       //ID発行ボタン
+    private Button btn_share;       //ID共有ボタン
+    private Button btn_findmode;   //検索モードに遷移するボタン
+    private EditText id_box;        //ユーザ名を入力するbox
+    private TextView text_idshow;   //サーバで発行されたIDを表示する領域
+
+    private String mac_add;         //Android端末のMacアドレスを保存
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
 
+        //xmlファイルとの紐づけ
         btn_issue = (Button)findViewById(R.id.button_issue);
         btn_share = (Button)findViewById(R.id.button_share);
         btn_findmode = (Button)findViewById(R.id.button_findmode);
         id_box = (EditText)findViewById(R.id.text_id);
         text_idshow = (TextView)findViewById(R.id.id_show);
 
+        //各ボタンのClickListenerの宣言
         btn_issue.setOnClickListener(issListener);
         btn_share.setOnClickListener(shaListener);
         btn_findmode.setOnClickListener(findListener);
@@ -40,7 +47,13 @@ public class RegActivity extends Activity {
     //ID発行ボタンの処理
     private View.OnClickListener issListener = new View.OnClickListener() {
         public void onClick(View v) {
-            text_idshow.setText("r3uhr3");
+            //text_idshow.setText("r3uhr3");
+            WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+            mac_add = wifiInfo.getMacAddress();
+            //mac address 確認用
+            text_idshow.setText(mac_add);
 
         }
     };
@@ -49,10 +62,12 @@ public class RegActivity extends Activity {
     private View.OnClickListener shaListener = new View.OnClickListener() {
         public void onClick(View v) {
             try {
+                //tmp
+                text_idshow.setText("r3uhr3");
                 Intent intent_sha = new Intent();
                 intent_sha.setAction(Intent.ACTION_SEND);
                 intent_sha.setType("text/plain");
-                intent_sha.putExtra(Intent.EXTRA_TEXT,text_idshow.getText().toString());
+                intent_sha.putExtra(Intent.EXTRA_TEXT,"集GO!しよう(*・・)ノ 私のIDは"+text_idshow.getText().toString()+"です．");
                 startActivity(intent_sha);
             } catch (Exception e){
                 Log.d("ActionSend","intent other app error");
