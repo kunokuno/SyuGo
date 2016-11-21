c1741aa01d061e25ff534e116a7c65a7da3f933d
Kai Aoki <s1620593@u.tsukuba.ac.jp>
2016-11-22 02:06:41 +0900

    テスト用にWifiDirect用のActivity遷移を追加

--- Syugo/app/src/main/java/jp/enpitsu/paseri/syugo/Start/StartActivity.java
+++ Syugo/app/src/main/java/jp/enpitsu/paseri/syugo/Start/StartActivity.java
@@ -5,12 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import jp.enpitsu.paseri.syugo.Lookfor.LookActivity;
-import jp.enpitsu.paseri.syugo.MainActivity;
 import jp.enpitsu.paseri.syugo.R;
 import jp.enpitsu.paseri.syugo.Registor.RegActivity;
+import jp.enpitsu.paseri.syugo.WiFiDirect.WiFiDirectActivity;
 
 /**
  * Created by owner on 2016/10/25.
@@ -19,6 +20,7 @@ public class StartActivity extends Activity {
 
     ImageButton imbtn_registmode;
     ImageButton imbtn_searchmode;
+    Button btn_wifidirectmode;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +28,11 @@ public class StartActivity extends Activity {
 
         imbtn_registmode = (ImageButton)findViewById(R.id.imageButton_regist);
         imbtn_searchmode = (ImageButton)findViewById(R.id.imageButton_search);
+        btn_wifidirectmode = (Button)findViewById(R.id.button_wifidirect);
 
         imbtn_registmode.setOnClickListener(regListener);
         imbtn_searchmode.setOnClickListener(seaListener);
+        btn_wifidirectmode.setOnClickListener(wfListner);
     }
 
     //ユーザ登録ボタンの処理
@@ -51,6 +55,17 @@ public class StartActivity extends Activity {
                 startActivity(intent_sea);
             } catch (Exception e) {
                 Log.d("StartActivity", "intent error to MainActivity");
+            }
+        }
+    };
+
+    private View.OnClickListener wfListner = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent_wf = new Intent(StartActivity.this, WiFiDirectActivity.class);
+            try {
+                startActivity(intent_wf);
+            } catch (Exception e) {
+                Log.d("StartActivity", e.toString());
             }
         }
     };
