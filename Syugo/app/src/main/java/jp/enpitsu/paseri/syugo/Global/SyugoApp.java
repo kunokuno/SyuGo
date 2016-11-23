e9b19391a70413aa40d4069da419896a13529843
Kai Aoki <s1620593@u.tsukuba.ac.jp>
2016-11-24 05:34:30 +0900

    セーブとロードを追加

--- Syugo/app/src/main/java/jp/enpitsu/paseri/syugo/Global/SyugoApp.java
+++ Syugo/app/src/main/java/jp/enpitsu/paseri/syugo/Global/SyugoApp.java
@@ -1,5 +1,7 @@
 package jp.enpitsu.paseri.syugo.Global;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import jp.enpitsu.paseri.syugo.Rader.LocationData;
@@ -9,16 +11,21 @@ import jp.enpitsu.paseri.syugo.Rader.LocationData;
  */
 
 class UserInfo {
-    public String name;
-    public String id;
+    public String name,id;
     public LocationData locationData;
+    public UserInfo(String name, String id, LocationData loc){
+        this.name = name;
+        this.id = id;
+        this.locationData = loc;
+    }
+
 }
 
 public class SyugoApp extends android.app.Application {
     private final String TAG = "SyuGoApp";
 
-    private UserInfo opponent;
-    private UserInfo self;
+    private UserInfo self = new UserInfo("self","init",new LocationData(0,0,0));
+    private UserInfo opponent = new UserInfo("opp","init",new LocationData(0,0,0));
 
     /* -----------------------------------------------------
 
@@ -28,12 +35,42 @@ public class SyugoApp extends android.app.Application {
 
     @Override
     public void onCreate() {
+        super.onCreate();
         Log.d(TAG,"SyuGo!");
+        loadUserInfo();
     }
 
     @Override
     public void onTerminate() {
-        Log.d(TAG,"解散...");
+        //呼ばれません（かなしい）
+        Log.d(TAG,"kaisan...");
+        super.onTerminate();
+    }
+
+    /* -----------------------------------------------------
+
+    save & load
+
+    ------------------------------------------------------- */
+
+    public void saveUserInfo (){
+        SharedPreferences data = getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = data.edit();
+        editor.putString("self_name",self.name);
+        editor.putString("self_id",self.id);
+        editor.putString("opp_name",opponent.name);
+        editor.putString("opp_id",opponent.id);
+        editor.apply();
+        Log.d(TAG,"data save");
+    }
+
+    private void loadUserInfo(){
+        SharedPreferences data = getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
+        self.name = data.getString("self_name","null");
+        self.id = data.getString("self_id","null");
+        self.name = data.getString("opp_name","null");
+        self.id = data.getString("opp_id","null");
+        Log.d(TAG,"data load");
     }
 
 
@@ -69,4 +106,10 @@ public class SyugoApp extends android.app.Application {
         return opponent.id;
     }
 
+    public void dump(){
+        Log.d(TAG,self.name);
+        Log.d(TAG,self.id);
+        Log.d(TAG,opponent.name);
+        Log.d(TAG,opponent.id);
+    }
 }
