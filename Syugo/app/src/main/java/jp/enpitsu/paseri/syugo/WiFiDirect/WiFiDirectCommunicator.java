a660ec2e9f5b1f0b061473d07f148f440c4007cb
Kai Aoki <s1620593@u.tsukuba.ac.jp>
2016-11-25 01:30:44 +0900

    wd通信での布石となるファイル群を追加（繋いでないので現時点では影響なし）

---
+++ Syugo/app/src/main/java/jp/enpitsu/paseri/syugo/WiFiDirect/WiFiDirectCommunicator.java
@@ -0,0 +1,72 @@
+package jp.enpitsu.paseri.syugo.WiFiDirect;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by Prily on 2016/11/24.
+ */
+
+public class WiFiDirectCommunicator implements WifiP2pManager.ConnectionInfoListener {
+
+    public static final String TAG = "wifi_direct_comnktor";
+
+    static final int PORT = 8898;
+    static final int TIMEOUT = 5000;
+    public static final int MESSAGE_READ = 0x400 + 1;
+    public static final int MY_HANDLE = 0x400 + 2;
+
+    WiFiDirectActivity activity;
+
+    WiFiDirectCommunicator(WiFiDirectActivity activity){
+        this.activity = activity;
+    }
+
+    @Override
+    public void onConnectionInfoAvailable(WifiP2pInfo p2pInfo) {
+        Thread handler = null;
+        /*
+         * The group owner accepts connections using a server socket and then spawns a
+         * client socket for every client. This is handled by {@code
+         * GroupOwnerSocketHandler}
+         */
+
+        if (p2pInfo.isGroupOwner) {
+            Log.d(TAG, "Connected as group owner");
+            try {
+                handler = new GroupOwnerSocketHandler(new Handler());
+                handler.start();
+            } catch (IOException e) {
+                Log.d(TAG,
+                        "Failed to create a server thread - " + e.getMessage());
+                return;
+            }
+        } else {
+            Log.d(TAG, "Connected as peer");
+            handler = new ClientSocketHandler(
+                    new Handler(),
+                    p2pInfo.groupOwnerAddress);
+            handler.start();
+        }
+    }
+}
