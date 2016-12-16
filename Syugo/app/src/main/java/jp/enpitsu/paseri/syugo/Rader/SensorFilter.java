4d8c74f4d9a48b5ac6fa1a049c87b6a07f0daa70
misaki_iyobe <16nm705a@vc.ibaraki.ac.jp>
2016-12-16 11:00:14 +0900

    変更を修正，元に戻るはず

--- Syugo/app/src/main/java/jp/enpitsu/paseri/syugo/Rader/SensorFilter.java
+++ Syugo/app/src/main/java/jp/enpitsu/paseri/syugo/Rader/SensorFilter.java
@@ -1,10 +1,8 @@
-package jp.enpitsu.paseri.syugo.Rader;
+package jp.enpitsu.paseri.syugo.AR;
 
 /**
  * Created by iyobe on 2016/09/29.
  */
-import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -14,15 +12,10 @@ public class SensorFilter {
     private ArrayList<Float> mSecond    = new ArrayList<Float>();
     private ArrayList<Float> mThrad     = new ArrayList<Float>();
 
-    private ArrayList<Float> mFirst_diff360     = new ArrayList<Float>();
-    private ArrayList<Float> mSecond_diff360    = new ArrayList<Float>();
-    private ArrayList<Float> mThrad_diff360     = new ArrayList<Float>();
-
     public int sampleCount=9;//サンプリング数
     public int sampleNum = 5;//サンプリングした値の使用値のインデックス
 
-    private float[] mParam = new float[3];// フィルタをかけた後の値
-    private float[] lastParam = new float[3];// 前回フィルタをかけた結果の値
+    private float[] mParam = new float[3];//フィルタをかけた後の値
 
     private boolean mSampleEnable=false;//規定のサンプリング数に達したか
 
@@ -46,63 +39,23 @@ public class SensorFilter {
         mSecond.add(sample[1]);
         mThrad.add(sample[2]);
 
-        mFirst_diff360.add(sample[0] -360f);
-        mSecond_diff360.add(sample[1]-360f);
-        mThrad_diff360.add(sample[2]-360f);
-
-
-
-        for( int i = 0; i < sample.length; ++i ) {
-            if( sample[i] < 0 ) sample[i] = sample[i] + 360;
-        }
-
-        if( sample[0] < 0 || sample[1] < 0 || sample[2] < 0 )
-            Log.d( "Sensor minus", sample[0] + "," + sample[1] + "," + sample[2] );
-
-
         //必要なサンプリング数に達したら
         if(mFirst.size() == sampleCount)
         {
-            // TODO: 0と360の境界をなくしたい
-
             //メディアンフィルタ(サンプリング数をソートして中央値を使用)かけて値を取得
             //その値にさらにローパスフィルタをかける
 
             ArrayList<Float> lst = (ArrayList<Float>) mFirst.clone();
-            ArrayList<Float> lst_diff = (ArrayList<Float>) mFirst_diff360.clone();
             Collections.sort(lst);
-            Collections.sort(lst_diff);
-            float tmp0 =  (mParam[0]*0.9f) + lst.get(sampleNum)*0.1f;
-            float tmp0_diff =(mParam[0]*0.9f) + lst_diff.get(sampleNum)*0.1f;
-
-            if ( Math.abs(mParam[0] - tmp0) > Math.abs(mParam[0] - tmp0_diff)  ) {
-                mParam[0] = tmp0_diff;
-            } else mParam[0] = tmp0;
-
+            mParam[0] =(mParam[0]*0.9f) + lst.get(sampleNum)*0.1f;
 
             lst = (ArrayList<Float>) mSecond.clone();
-            lst_diff = (ArrayList<Float>) mSecond_diff360.clone();
             Collections.sort(lst);
-            Collections.sort(lst_diff);
-            float tmp1 =  (mParam[1]*0.9f) + lst.get(sampleNum)*0.1f;
-            float tmp1_diff =(mParam[1]*0.9f) + lst_diff.get(sampleNum)*0.1f;
-
-            if ( Math.abs(mParam[1] - tmp0) > Math.abs(mParam[1] - tmp0_diff)  ) {
-                mParam[1] = tmp0_diff;
-            } else mParam[1] = tmp0;
-
+            mParam[1] = (mParam[1]*0.9f) +lst.get(sampleNum)*0.1f;
 
             lst = (ArrayList<Float>) mThrad.clone();
-            lst_diff = (ArrayList<Float>) mThrad_diff360.clone();
             Collections.sort(lst);
-            Collections.sort(lst_diff);
-            float tmp2 =  (mParam[2]*0.9f) + lst.get(sampleNum)*0.1f;
-            float tmp2_diff =(mParam[2]*0.9f) + lst_diff.get(sampleNum)*0.1f;
-
-            if ( Math.abs(mParam[2] - tmp0) > Math.abs(mParam[2] - tmp0_diff)  ) {
-                mParam[2] = tmp0_diff;
-            } else mParam[2] = tmp0;
-
+            mParam[2] = (mParam[2]*0.9f) +lst.get(sampleNum)*0.1f;
 
             mSampleEnable = true;
 
@@ -110,11 +63,6 @@ public class SensorFilter {
             mFirst.remove(0);
             mSecond.remove(0);
             mThrad.remove(0);
-
-            mFirst_diff360.remove(0);
-            mSecond_diff360.remove(0);
-            mThrad_diff360.remove(0);
-
         }
     }
 
